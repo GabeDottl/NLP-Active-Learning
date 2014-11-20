ff3e8ecca707513e44300088ecf974e553119a88
GabeDottl <GabeDottl@gmail.com>
2014-11-20 11:44:25 -0600

    init

---
+++ activeLearning.py
@@ -0,0 +1,7 @@
+import numpy as np
+from sklearn.decomposition import PCA
+X = np.array([[-1, -1], [-2, -1], [-3, -2], [1, 1], [2, 1], [3, 2]])
+pca = PCA(n_components=2)
+pca.fit(X)
+PCA(copy=True, n_components=2, whiten=False)
+print(pca.explained_variance_ratio_) 
